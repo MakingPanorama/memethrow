e89a4057434fc2ae37c236d2c836c0a59bccca85
MakingPanorama <50879571+MakingPanorama@users.noreply.github.com>
2020-08-06 21:35:31 +0300

    Added Dagon

---
+++ scripts/vscripts/items/dagon.lua
@@ -0,0 +1,22 @@
+function Dagon( keys )
+	local caster = keys.caster
+	local target = keys.target
+	local ability = keys.ability
+
+	local damage = ability:GetSpecialValueFor("damage_tooltip")
+
+	local dagon_pfx = ParticleManager:CreateParticle(particle, PATTACH_RENDERORIGIN_FOLLOW, source)
+	ParticleManager:SetParticleControlEnt(dagon_pfx, 0, source, PATTACH_POINT_FOLLOW, "attach_attack1", source:GetAbsOrigin(), false)
+	ParticleManager:SetParticleControlEnt(dagon_pfx, 1, target, PATTACH_POINT_FOLLOW, "attach_hitloc", target:GetAbsOrigin(), false)
+	ParticleManager:SetParticleControl(dagon_pfx, 2, Vector(damage, 0, 0))
+	ParticleManager:SetParticleControl(dagon_pfx, 3, color)
+	ParticleManager:ReleaseParticleIndex(dagon_pfx)
+
+	ApplyDamage( {
+		victim = target,
+		attacker = caster,
+		ability = ability,
+		damage = damage,
+		damage_type = DAMAGE_TYPE_MAGICAL
+	} )
+end
